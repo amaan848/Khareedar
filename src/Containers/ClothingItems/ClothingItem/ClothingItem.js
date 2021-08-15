459f220f34bd2ca816052620477fce70abdaee51
Amaan Iqbal <amaaniqbal@MacBook-Pro.local>
2021-08-15 18:00:41 +0530

    added components , buttons and handlers

--- src/Containers/ClothingItems/ClothingItem/ClothingItem.js
+++ src/Containers/ClothingItems/ClothingItem/ClothingItem.js
@@ -1,52 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import shirt from '../../../assets/shirt.jpeg';
-import cap from '../../../assets/cap.jpeg';
-import hoodie from '../../../assets/hoodie.jpeg';
-import jeans from '../../../assets/jeans.jpeg';
-import shoes from '../../../assets/shoes.jpeg';
-import trouser from '../../../assets/trouser.png';
+import shirtimage from '../../../assets/shirt.jpeg';
+import capimage  from '../../../assets/cap.jpeg';
+import hoodieimage  from '../../../assets/hoodie.jpeg';
+import jeansimage  from '../../../assets/jeans.jpeg';
+import shoesimage  from '../../../assets/shoes.jpeg';
+import trouserimage  from '../../../assets/trouser.png';
 
 import classes from './ClothingItem.module.css';
 
-const ClothingItem = (props) => {
-    let imageURL = null;
-    switch(props.item){
+
+class ClothingItem extends Component{
+
+   render(){       
+       let imageURL = null;
+    switch(this.props.item){
         case 'shirt':
-            imageURL=shirt;
+            imageURL=shirtimage ;
             break;
         case 'cap':
-            imageURL=cap;
+            imageURL=capimage ;
             break;
         case 'hoodie':
-            imageURL=hoodie;
+            imageURL=hoodieimage ;
             break;
         case 'jeans':
-            imageURL=jeans;
+            imageURL=jeansimage ;
             break;
         case 'shoes':
-            imageURL=shoes;
+            imageURL=shoesimage ;
             break;
         case 'trouser':
-            imageURL=trouser;
+            imageURL=trouserimage ;
             break;
         default:
             imageURL=null;
     }
 
-   
+    const decreaseQuantity = ()=>{
+      let presentQuan = this.props.quantity
+      this.props.setQuantity(presentQuan - 1);
+    }
+
+    const increaseQuantity = ()=>{
+      let presentQuan = this.props.quantity
+      this.props.setQuantity(presentQuan + 1);
+    }
+
+    const isDisabled = this.props.quantity === 0 ? true : false
+
     return (
         <div>
             <div className={classes.counterItems}>
-                <div className={classes.signs}>-</div>
+                <button className={classes.signs} onClick={decreaseQuantity} disabled={isDisabled}>-</button>
                 <div className={classes.itemContainer}>
                     <img src={imageURL} alt="Could'nt load"/>
                 </div>
-                <div className={classes.signs}>+</div>
+                <button className={classes.signs} onClick={increaseQuantity}>+</button>
             </div>
-            <p className={classes.nums}>{props.number}</p>
+            <p className={classes.nums}>Quantity : {this.props.quantity}</p>
+            <hr/>
         </div>
     )
+}
 }
 
 
--- src/Containers/ClothingItems/ClothingItems.js
+++ src/Containers/ClothingItems/ClothingItems.js
@@ -2,31 +2,89 @@ import React, { Component } from 'react';
 import ClothingItem from './ClothingItem/ClothingItem';
 import classes from './ClothingItems.module.css';
 
+
 class ClothingItems extends Component {
     state = {
-        itemsPicked : {
-            shirt : 1,
-            trouser : 1,
-            shoes : 1,
-            cap : 1,
-            hoodie : 1,
-            jeans : 1
-        }
+        itemsPicked: {
+            'shirt': 0,
+            'trouser': 0,
+            'shoes': 0,
+            'cap': 0,
+            'hoodie': 0,
+            'jeans': 0
+        },
+        totalPrice : 0,
+        checkoutContinued : false
     };
 
+      PRICE_CHART = {
+            'shirt': 5.4,
+            'trouser': 7.9,
+            'shoes': 15.0,
+            'cap': 3.1,
+            'hoodie': 5.6,
+            'jeans': 11.3
+        };
+
     render() {
-        const items = Object.keys(this.state.itemsPicked).map((item => {
-           return <ClothingItem 
-            item={item} 
-            number={this.state.itemsPicked[item]} 
-            key={item}/>
-        }))
+        const  setQuantity = (item) => (quantity) => {
+            const newState = {
+                itemsPicked: {
+                    ...this.state.itemsPicked,
+                    [item]: quantity
+                },
+                totalPrice: quantity > this.state.itemsPicked[item] ? 
+                            this.state.totalPrice + (this.PRICE_CHART[item]) : 
+                            this.state.totalPrice - (this.PRICE_CHART[item])
+            }
+            this.setState(newState)
+        }
+        const itemsPicked  = ['shirt','trouser','shoes','cap','hoodie','jeans'];
+        const items = itemsPicked.map((item =>
+            <ClothingItem
+                item={item}
+                key={item}
+                quantity={this.state.itemsPicked[item]}
+                setQuantity={setQuantity(item)}
+            />
+        ))
+
+        let orderDisabled = true;
+        itemsPicked.map((item) => {
+            if(this.state.itemsPicked[item] === 0 && orderDisabled){
+                orderDisabled = true;
+            }
+            else{
+                orderDisabled = false;
+            }
+        })
+
+        const cancelCheckout = ()=>{
+            window.history.go()
+        }
+
+        const continueCheckout = ()=>{
+            this.setState({checkoutContinued : true})
+            console.log(this.state.checkoutContinued)
+        }
+        let containerClass = ['allItems'].concat(this.state.checkoutContinued ? 'overlay' : null).join(' ');
         return (
             <div className={classes.allItems}>
                 {items}
+                <div className={classes.priceBar}>BAG PRICE : {this.state.totalPrice}$</div>
+                <div className={classes.checkout}>
+                    <button className={classes.cancelButton} onClick={cancelCheckout}>CANCEL</button>
+                    <button 
+                        disabled={orderDisabled} 
+                        className={classes.orderButton}
+                        onClick={continueCheckout}>
+                            CHECKOUT
+                    </button>
+                </div>
             </div>
-        )
+        );
     }
-}
+};
+
 
 export default ClothingItems;
